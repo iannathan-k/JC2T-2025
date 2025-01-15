80d2556e97f46788b83a3ce15a7bb6de05b96785
iannathan-k <ianmtv@gmail.com>
2025-01-15 10:45:11 +0700

    binary search

---
+++ 25-01-15/binarySearch.py
@@ -0,0 +1,25 @@
+array = [1, 2, 4, 8, 16, 32, 64, 128]
+
+low = 0
+high = len(array) - 1
+search_element = int(input("Num to search?: "))
+found = False
+flag = True
+
+while flag:
+    mid  = (low + high) // 2
+    if array[mid] == search_element:
+        flag = False
+        found = True
+    elif search_element > array[mid]:
+        low = mid + 1
+    else:
+        high = mid - 1
+
+    if mid == (low + high) // 2:
+        flag = False
+
+if found:
+    print("Found!")
+else:
+    print("Not Found")
