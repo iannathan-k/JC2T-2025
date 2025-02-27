d8174561e2ca176290eeeed890c5eb5b62548d08
iannathan-k <ianmtv@gmail.com>
2025-02-27 13:45:16 +0700

    binary search recursive

--- 25-02-27/recursion.py
+++ 25-02-27/recursion.py
@@ -17,14 +17,28 @@ def countCharacters(string, char):
         return countCharacters(string[1:], char)
 
 def fibbonaci(num):
-    if num <= 0: return 1
+    if num <= 1: return num
     return fibbonaci(num - 1) + fibbonaci(num - 2)
 
-def fibbonaci(num):
-    if num == 0: return 1
-    return num + fibbonaci(num - 1)
+# def fibbonaci(num):
+#     if num == 0: return 1
+#     print(num)
+#     return num + fibbonaci(num - 1)
+
+def binarySearch(array, lower_bound, upper_bound, num):
+    if upper_bound < lower_bound: return -1
+
+    mid = (lower_bound + upper_bound) // 2
+    if num == array[mid]: return mid
+    if num > array[mid]: return binarySearch(array, mid + 1, upper_bound, num)
+    if num < array[mid]: return binarySearch(array, lower_bound, mid - 1, num)
 
 print(sumOfArray([1, 2, 3, 4, 5]))
 print(reverseString("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 print(countCharacters("Hello World", 'l'))
 print(fibbonaci(3))
+print(binarySearch([1, 4, 7, 9, 10, 12], 0, 5, 123))
+
+# 1 1 2 3 5 8 13
+for i in range(10):
+    print(fibbonaci(i))
